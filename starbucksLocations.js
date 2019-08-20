20aafd217e6630748ca80abb36bba3efb6841a01
oyekunle-mark <oyekunlemac@gmail.com>
2019-08-20 15:45:54 +0100

    Built the object

--- starbucksLocations.js
+++ starbucksLocations.js
@@ -6,6 +6,33 @@ const rl = readline.createInterface({
   crlfDelay: Infinity,
 });
 
+const starbucksLocations = [];
+
+const imgaes = [
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiLqR1M8Oq2awl4d6DrzDOeN90CZCIHZA6M5YI6GIg77mQgH4AwQ',
+  'https://purewows3.imgix.net/images/articles/2017_01/bespoke_coworking_spaces_san_francisco.png?auto=format,compress&cs=strip',
+  'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Feustaciahuen%2Ffiles%2F2017%2F03%2FNeuehouse-3-1200x675.jpg',
+  'https://www.rent24.com/wp-content/uploads/coworking-space-berlin-schoeneberg.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9Gy7PX83f8LYEiwxo_rpspew0gy_AaWxI3IiSQWWEX62u7bmQqw',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCO9ZF-XM5w2290UTDjDq-KfOvNo71cWB0CuaEQK_Y22d5ch4QJg',
+  'http://thespaces.com/wp-content/uploads/2017/02/Primary-Co-Working-NYC.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2dDHB_nliR3160ubZgp6J9A3nfPUsHMXcoVGwgb4x3oBzgGNU',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQN7aBKuIsNaZaqBosrlp3oz3ziEmI2dhtzTdijGrcmwWIYSBQs',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbifgcp_29eEnE5BozbdrZweuFHtvN-sJRsooOtmBrDAQqHSDW',
+];
+
 rl.on('line', line => {
-  console.log(`Line from file: ${line}`);
+  const lineArr = line.split(',');
+  const starbucksObj = {
+    name: lineArr[2],
+    description: 'A Starbucks shop to work out from',
+    image_url: 'https://image.flaticon.com/icons/svg/201/201559.svg',
+    address: `${lineArr[4]}, ${lineArr[5]}, ${lineArr[6]}, ${
+      lineArr[7]
+    }`.replace(/"/g, ''),
+    latitude: lineArr[lineArr.length - 2],
+    longitude: lineArr[lineArr.length - 1],
+  };
+
+  console.log(starbucksObj);
 });
